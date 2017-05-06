844beb84db5265e8726f8039959cd622246f9421
Greg Hazen <ghazen1@gmail.com>
2017-05-06 15:29:29 -0500

    Added example of purely object-oriented

--- src/main/scala/C_Functions.scala
+++ src/main/scala/C_Functions.scala
@@ -2,4 +2,7 @@ object Functions {
   def printSomething: Unit = println("Something")
 
   def add(a: Int, b: Int): Int = a + b
+
+  // Note Scala is purely object-oriented
+  def addIsActuallyAFunction(a: Int, b: Int): Int = a.+(b)
 }
--- src/test/scala/C_FunctionsSpec.scala
+++ src/test/scala/C_FunctionsSpec.scala
@@ -5,4 +5,8 @@ class FunctionsSpec extends FlatSpec {
   "add" should "add ints" in {
     assert(add(1, 2) == 3)
   }
+
+  "addIsActuallyAFunction" should "be the same as add" in {
+    assert(add(1, 2) == addIsActuallyAFunction(1, 2))
+  }
 }
