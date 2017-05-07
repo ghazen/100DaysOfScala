1596f67f9e73e64aaa92dfe8f2fdc5a4da3c4fbd
Greg Hazen <ghazen1@gmail.com>
2017-05-07 15:54:56 -0500

    Added quote about not enforcing functional in Scala

--- src/main/scala/B_Variables.scala
+++ src/main/scala/B_Variables.scala
@@ -10,3 +10,11 @@ object Variables {
   // Won't compile
 //  def incrementImmutableVariable = immutableVariable = immutableVariable + 1
 }
+
+//"The Scala perspective, however, is that val and var are
+// just two different tools in your toolbox, both useful,
+// neither inherently evil. Scala encourages you to lean
+// towards vals, but ultimately reach for the best tool
+// given the job at hand."
+// -- Programming in Scala 3rd Ed
+//    Martin Odersky, Lex Spoon, Bill Venners
