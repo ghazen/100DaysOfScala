bdb964bf060b7c0d8571e2898646d3d924d7ac7d
Greg Hazen <ghazen1@gmail.com>
2017-05-07 15:31:18 -0500

    Added examples of first class functions

--- src/main/scala/C_Functions.scala
+++ src/main/scala/C_Functions.scala
@@ -5,4 +5,7 @@ object Functions {
 
   // Note Scala is purely object-oriented
   def addIsActuallyAFunction(a: Int, b: Int): Int = a.+(b)
+
+  def placeholders = (_: Int) + (_: Int)
+  def placeholdersWithParams = (a: Int, b: Int) => a + b
 }
--- src/test/scala/C_FunctionsSpec.scala
+++ src/test/scala/C_FunctionsSpec.scala
@@ -9,4 +9,16 @@ class FunctionsSpec extends FlatSpec {
   "addIsActuallyAFunction" should "be the same as add" in {
     assert(add(1, 2) == addIsActuallyAFunction(1, 2))
   }
+
+  "placeholders" should "take parameters for underscores" in {
+    assert(placeholders(1, 2) == 3)
+    assert(placeholdersWithParams(1, 2) == 3)
+  }
+
+  it should "be reusable" in {
+    val list = List(1, 2, 3)
+    assert(list.reduceLeft(placeholders) == 6)
+    assert(list.reduceLeft(placeholdersWithParams) == 6)
+    assert(list.reduceLeft(_ + _) == 6)
+  }
 }
